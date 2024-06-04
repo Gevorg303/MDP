b4bdd9b4eaed220e08925ccdca6cc227bcd51352
Kotan787 <kotan78744@gmail.com>
2024-06-04 18:08:56 +0300

    Добавлены сервисы для индикатора и варианта ответа

--- src/main/java/com/example/smart_test/mapper/ResponseOptionMapperImpl.java
+++ src/main/java/com/example/smart_test/mapper/ResponseOptionMapperImpl.java
@@ -2,11 +2,11 @@ package com.example.smart_test.mapper;
 
 import com.example.smart_test.domain.ResponseOption;
 import com.example.smart_test.dto.ResponseOptionDto;
-import com.example.smart_test.mapper.api.ResponceOptionMapperInterface;
+import com.example.smart_test.mapper.api.ResponseOptionMapperInterface;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ResponseOptionMapperImpl implements ResponceOptionMapperInterface {
+public class ResponseOptionMapperImpl implements ResponseOptionMapperInterface {
     @Override
     public ResponseOptionDto toDTO(ResponseOption entity) {
         ResponseOptionDto dto = new ResponseOptionDto();
--- src/main/java/com/example/smart_test/mapper/api/ResponseOptionMapperInterface.java
+++ src/main/java/com/example/smart_test/mapper/api/ResponseOptionMapperInterface.java
@@ -3,7 +3,7 @@ package com.example.smart_test.mapper.api;
 import com.example.smart_test.domain.ResponseOption;
 import com.example.smart_test.dto.ResponseOptionDto;
 
-public interface ResponceOptionMapperInterface {
+public interface ResponseOptionMapperInterface {
     public ResponseOptionDto toDTO(ResponseOption entity);
     public ResponseOption toEntity(ResponseOptionDto dto);
 }
