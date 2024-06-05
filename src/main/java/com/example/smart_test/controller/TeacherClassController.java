cea24a377c2498c72ce79e720c5f1f9edbd74cc7
Kotan787 <kotan78744@gmail.com>
2024-06-05 15:48:11 +0300

    Добавлены контроллеры для сущности TypeTask, TypeTest, TeacherClass, TeacherEducationalInstitution, TestResults

---
+++ src/main/java/com/example/smart_test/controller/TeacherClassController.java
@@ -0,0 +1,28 @@
+package com.example.smart_test.controller;
+
+
+import com.example.smart_test.dto.TeacherClassDto;
+import com.example.smart_test.service.api.TeacherClassServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("TeacherClass")
+public class TeacherClassController {
+    @Autowired
+    private TeacherClassServiceInterface teacherClassService;
+    @PostMapping("/add")
+    public TeacherClassDto addTeacherClassDto(@RequestBody TeacherClassDto teacherClassDto){
+        return teacherClassService.addTeacherClassDto(teacherClassDto);
+    }
+    @DeleteMapping("/delete")
+    public void deleteTeacherClassDto(@RequestBody TeacherClassDto teacherClassDto){
+        teacherClassService.deleteTeacherClassDto(teacherClassDto);
+    }
+    @GetMapping("/all")
+    public List<TeacherClassDto> getTeacherClassDto(){
+        return teacherClassService.getAllTeacherClasses();
+    }
+}
