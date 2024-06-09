d4cacae5d9f3057a0dcdd870f2ff61f4d1368f83
Gev <gevorg112003@mail.ru>
2024-06-09 21:24:21 +0300

    Сделал переходы

--- src/main/java/com/example/smart_test/controller/HomeController.java
+++ src/main/java/com/example/smart_test/controller/HomeController.java
@@ -2,12 +2,22 @@ package com.example.smart_test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test-smart")
 public class HomeController {
 
+    @GetMapping("/login")
+    public String openLogin() {
+        return "login";
+    }
     @GetMapping("/main")
-    public String showHomePage() {
+    public String openMain() {
         return "main";
+    }
+    @GetMapping("/help_login")
+    public String openHelpLogin() {
+        return "help_login";
     }
 }
